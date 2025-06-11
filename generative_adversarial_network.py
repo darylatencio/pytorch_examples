cf85ec82d66fcb0bc785f6c813b86c7622279f2b
Daryl Atencio <datencio1973@gmail.com>
2025-06-10 18:08:07 -0600

    generative adversarial network example

---
+++ generative_adversarial_network.py
@@ -0,0 +1,242 @@
+import matplotlib.pyplot as plot
+import numpy as np
+import os
+import tempfile
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+
+#--------------------------------------------------------------------------------------------------
+#+
+#-
+class generative_adversarial_model():
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    #----------------------------------------------------------------------------------------------
+    #+
+    #-    
+    def __init__(self, batch_size=100, dataset=None, discriminator_file=None, generator_file=None,
+                 hidden_size=256, image_size=784, latent_size=64, learning_rate=0.0002,
+                 num_epoch=100, relu_neg_slope=0.2, root_folder=None, shuffle=True,
+                 verbose=False):
+        self.batch_size = batch_size
+        self.dir = os.path.join(os.path.join(tempfile.gettempdir(),"data"),"pytorch") \
+            if (root_folder == None) else root_folder
+        self.dir_sample = os.path.join(self.dir, "ga_samples")
+        if not os.path.exists(self.dir_sample):
+            os.mkdir(self.dir_sample)
+        self.hidden_size = hidden_size
+        self.fig, self.grid = plot.subplots(1, 2, num="Generative Adversarial Network")
+        self.image_size = image_size
+        self.latent_size = latent_size
+        self.learning_rate = learning_rate
+        self.n_epoch = num_epoch
+        self.verbose = verbose
+        self.initialize_dataloader(dataset=dataset, shuffle=shuffle)
+        self.initialize_network(discriminator_file, generator_file, relu_neg_slope=relu_neg_slope)
+        str = f"initialized:\n  batch_size: {self.batch_size}\n  device: {self.device}\n"+ \
+            f"  hidden size: {self.hidden_size}\n  image size: {self.image_size}\n"+ \
+            f"  latent size: {self.latent_size}\n  learning rate: {self.learning_rate}\n"+ \
+            f"  number of epochs: {self.n_epoch}\n  root folder: {self.dir}"
+        self.print(str)
+
+    #----------------------------------------------------------------------------------------------
+    #+
+    #-
+    def generate_test_image(self, img):
+        img_out = img.reshape(img.size(0), 1, self.dim[0], self.dim[1])
+        img_out = torchvision.utils.make_grid(img_out)
+        img_out = img_out.cpu().numpy() if img_out.is_cuda else img_out.numpy()
+        img_out = np.transpose(img_out, axes=(1,2,0))
+        return img_out
+
+    #----------------------------------------------------------------------------------------------
+    #+
+    #-    
+    def initialize_dataloader(self, dataset=None, shuffle=True):
+        self.print("initializing data loaders...")
+        download = os.path.exists(os.path.join(self.dir, "MNIST","raw"))
+        if (dataset == None):
+            tf = transforms.Compose([transforms.ToTensor(),
+                                     transforms.Normalize(mean=[0.5], std=[0.5])])
+            dataset = torchvision.datasets.MNIST(download=download, root=self.dir,
+                                            train=True, transform=tf)
+        self.data_loader = torch.utils.data.DataLoader(batch_size=self.batch_size,
+                                                       dataset=dataset, shuffle=shuffle)
+
+    #----------------------------------------------------------------------------------------------
+    #+
+    #-
+    def initialize_discriminator(self, file=None, relu_neg_slope=0.2):
+        self.discriminator = nn.Sequential(
+             nn.Linear(self.image_size, self.hidden_size),
+             nn.LeakyReLU(relu_neg_slope),
+             nn.Linear(self.hidden_size, self.hidden_size),
+             nn.LeakyReLU(relu_neg_slope),
+             nn.Linear(self.hidden_size, 1),
+             nn.Sigmoid())
+        self.discriminator = self.discriminator.to(self.device)
+        self.file_discriminator = \
+            os.path.join(os.path.dirname(__file__), "model",
+                         "ga_network_discriminator.dct") \
+            if (file == None) else file
+        if os.path.exists(self.file_discriminator):
+            self.print(f"  loading existing discriminator dictionary:"+
+                       f" {self.file_discriminator}")
+            self.discriminator.load_state_dict(torch.load(self.file_discriminator,
+                                                          weights_only=True))
+        self.opt_discriminator = torch.optim.Adam(self.discriminator.parameters(),
+                                                  lr=self.learning_rate)
+
+    #----------------------------------------------------------------------------------------------
+    #+
+    #-
+    def initialize_generator(self, file=None):
+        self.generator = nn.Sequential(
+             nn.Linear(self.latent_size, self.hidden_size),
+             nn.ReLU(),
+             nn.Linear(self.hidden_size, self.hidden_size),
+             nn.ReLU(),
+             nn.Linear(self.hidden_size, self.image_size),
+             nn.Tanh())
+        self.generator = self.generator.to(self.device)
+        self.file_generator = \
+            os.path.join(os.path.dirname(__file__), "model", "ga_network_generator.dct") \
+            if (file == None) else file
+        if os.path.exists(self.file_generator):
+            self.print(f"  loading existing generator dictionary: {self.file_generator}")
+            self.generator.load_state_dict(torch.load(self.file_generator, weights_only=True))
+        self.opt_generator = torch.optim.Adam(self.generator.parameters(), lr=self.learning_rate)
+
+    #----------------------------------------------------------------------------------------------
+    #+
+    #-
+    def initialize_network(self, discriminator_file=None, generator_file=None,
+                           relu_neg_slope=None):
+        self.print("initializing discriminator/generator...")
+        self.initialize_discriminator(file=discriminator_file, relu_neg_slope=relu_neg_slope)
+        self.initialize_generator(file=generator_file)
+        self.fn_loss = nn.BCELoss()
+
+    #----------------------------------------------------------------------------------------------
+    #+
+    #-    
+    def print(self, str):
+        if not self.verbose:
+            return None
+        print("[GA_NETWORK] "+str)
+
+    #----------------------------------------------------------------------------------------------
+    #+
+    #-    
+    def reset_grad(self):
+        self.opt_discriminator.zero_grad()
+        self.opt_generator.zero_grad()
+
+    #----------------------------------------------------------------------------------------------
+    #+
+    #-    
+    def save(self, discriminator_file=None, generator_file=None):
+        file_discriminator = self.file_discriminator if (discriminator_file == None) else \
+            discriminator_file
+        file_generator = self.file_generator if (generator_file == None) else \
+            generator_file
+        self.print(f"saving model files:\n  {file_discriminator}\n  {file_generator}")
+        torch.save(self.discriminator.state_dict(), file_discriminator)
+        torch.save(self.generator.state_dict(), file_generator)
+
+    #----------------------------------------------------------------------------------------------
+    #+
+    #-    
+    def test(self, from_train=False):
+        self.print("testing model...")
+        plot.cla()
+    # real image
+        g = self.grid[0]
+        g.axis("off")
+        g.text(0.0, 1.01, "real data", color="#000000", transform=g.transAxes)
+        img, void = next(iter(self.data_loader))
+        g.imshow(self.generate_test_image(img), cmap="gray")
+    # fake image
+        g = self.grid[1]
+        g.axis("off")
+        g.text(0.0, 1.01, "fake data", color="#000000", transform=g.transAxes)
+        z = torch.randn(self.batch_size, self.latent_size).to(self.device)
+        img = self.generator(z)
+        g.imshow(self.generate_test_image(img), cmap="gray")
+        if from_train:
+            plot.ion()
+        if self.fig.get_visible():
+            plot.draw()
+        else:
+            plot.show()
+        if from_train:
+            plot.pause(0.25)
+        return None
+
+    #----------------------------------------------------------------------------------------------
+    #+
+    #-    
+    def train(self, num_epoch=None, save_epochs=False, save_models=False, show_progress=False):
+        self.print("training discriminator/generator...")
+        n_step = len(self.data_loader)
+        n_epoch = self.n_epoch if (num_epoch == None) else num_epoch
+        for i_epoch in range(n_epoch):
+            for i_img, (img, void) in enumerate(self.data_loader):
+                self.dim = img.shape[2:4]
+                img = img.reshape(self.batch_size, -1).to(self.device)
+                label_real = torch.ones(self.batch_size, 1).to(self.device)
+                label_fake = torch.zeros(self.batch_size, 1).to(self.device)
+            # train the discriminator
+                # real
+                output = self.discriminator(img)
+                d_loss_real = self.fn_loss(output, label_real)
+                d_score_real = output
+                # fake
+                z = torch.randn(self.batch_size, self.latent_size).to(self.device)
+                img_fake = self.generator(z)
+                output = self.discriminator(img_fake)
+                d_loss_fake = self.fn_loss(output, label_fake)
+                d_score_fake = output
+                d_loss = d_loss_real + d_loss_fake
+                self.reset_grad()
+                d_loss.backward()
+                self.opt_discriminator.step()
+            # train the generator
+                z = torch.randn(self.batch_size, self.latent_size).to(self.device)
+                g_img_fake = self.generator(z)
+                g_output_fake = self.discriminator(g_img_fake)
+                g_loss = self.fn_loss(g_output_fake, label_real)
+                self.reset_grad()
+                g_loss.backward()
+                self.opt_generator.step()
+                if ((i_img+1) % 100 == 0):
+                    self.print(f"  epoch [{i_epoch+1}/{n_epoch}], "+
+                               f"step [{i_img+1}/{n_step}], "+
+                               f"d_loss: {d_loss.item():.4f}, "+
+                               f"g_loss: {g_loss.item():.4f}, "+
+                               f"d(x): {d_score_real.mean().item():.4f}, "+
+                               f"d(g(z)): {d_score_fake.mean().item():.4f}")
+            if show_progress:
+                self.test(from_train=True)
+            if save_epochs:
+                self.save()
+        if save_models:
+            self.save()
+
+#--------------------------------------------------------------------------------------------------
+#+
+#-
+def test_ga_model():
+    dir = "C:\\data"
+    ga_model = generative_adversarial_model(root_folder=dir, verbose=True)
+    ga_model.train(num_epoch=100, save_epochs=True, save_models=True, show_progress=True)
+    ga_model.test()
+
+#--------------------------------------------------------------------------------------------------
+#+
+#-
+if (__name__ == "__main__"):
+    test_ga_model()
